e60750c06c21a8615dc1ce75ef4419bc62958010
Mark James Felecio <147979567+mjfelecio@users.noreply.github.com>
2025-09-19 09:28:34 +0800

    feat: Redesign home page
    - add bootstrap icon as a dependency
    - add new color configs for the color pallete
    - Edited bg colors of the buttons and cards

--- resources/views/components/title.blade.php
+++ resources/views/components/title.blade.php
@@ -1,2 +1,2 @@
-<h1 class="text-2xl font-semibold">ACLC Virtual Library</h1>
+<h1 class="text-4xl font-semibold">ACLC Virtual Library</h1>
 <!-- Simplicity is the ultimate sophistication. - Leonardo da Vinci -->
